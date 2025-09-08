97020d448a67fae1340ff327a8c8232a87803a4e
choihooo <hochoi8621@naver.com>
2025-09-09 02:40:35 +0900

    fix: 스왑 페이지 스타일 수정

--- src/pages/swap/SwapPage.tsx
+++ src/pages/swap/SwapPage.tsx
@@ -11,19 +11,10 @@ const SwapTitle = styled.h1`
   margin-bottom: 16px;
 `;
 
-const SwapDescription = styled.p`
-  color: #6b7280;
-  line-height: 1.5;
-`;
-
 function SwapPage() {
   return (
     <SwapContainer>
-      <SwapTitle>스왑</SwapTitle>
-      <SwapDescription>
-        다른 사용자와 상품을 교환할 수 있는 공간입니다. 원하는 상품을
-        찾아보세요!
-      </SwapDescription>
+      <SwapTitle>스왑</SwapTitle>=
       <SwipeCards />
     </SwapContainer>
   );
